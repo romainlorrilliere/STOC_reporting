ebf5947c20a077a054b25fc0e4803a2031a2a7f2
Romain Lorrilliere <romainlorrilliere@gmail.com>
2020-10-27 16:17:04 +0100

    Update main_reporting.r

--- main_reporting.r
+++ main_reporting.r
@@ -5,4 +5,4 @@ source("functions/fun_rmd_latin1.r")
 source("functions/fun_mainSTOCreporting_latin1.r")
 source("functions/fun_stoc_preparation_data_latin1.r")
 
-#mainSTOCreporting(importationData="brut",all=FALSE,local=TRUE,site="203",pdf_local=TRUE,abondanceRelative=TRUE ,variationAbondance=TRUE,variationAbondanceEspece=TRUE,productivite=TRUE,productiviteEspece=TRUE,conditionCorporelle=TRUE,retour=TRUE,onlyNew=TRUE)
+# mainSTOCreporting(importationData="3sessions",all=FALSE,local=TRUE,site="204",lastYear = 2019, pdf_local=TRUE,abondanceRelative=TRUE ,variationAbondance=TRUE,variationAbondanceEspece=TRUE,productivite=TRUE,productiviteEspece=TRUE,conditionCorporelle=TRUE,retour=TRUE,onlyNew=TRUE)
